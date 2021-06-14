ab692d16cfba4862b5e8a5e828dd60f9cb6c1ebf
Toppedyk <Tayloroppedyk@yahoo.com>
2021-06-14 11:02:38 -0600

    Project startup

---
+++ DaNetwork.Client/src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:5001' : ''
+export const domain = 'taylor-oppedyk.us.auth0.com'
+export const audience = 'https://MyNetwork.com'
+export const clientId = 'Zy5ExeaHVIWjwpmAFG24Bso8QlpTajCx'
