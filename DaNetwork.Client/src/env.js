8de7048f256754cbabe0fd2c09fc0974ec4b08f5
Taylor Oppedyk <tayloroppedyk@yahoo.com>
2021-06-14 18:54:19 -0600

    Delete env.js

--- DaNetwork.Client/src/env.js
+++
@@ -1,5 +0,0 @@
-export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'https://localhost:5001' : ''
-export const domain = 'taylor-oppedyk.us.auth0.com'
-export const audience = 'https://MyNetwork.com'
-export const clientId = 'Zy5ExeaHVIWjwpmAFG24Bso8QlpTajCx'
