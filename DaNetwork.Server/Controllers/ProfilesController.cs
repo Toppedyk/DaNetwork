cb7483519938d734d997cda74850300432351289
Toppedyk <Tayloroppedyk@yahoo.com>
2021-06-21 12:10:36 -0600

    readonly for profiles

--- DaNetwork.Server/Controllers/ProfilesController.cs
+++ DaNetwork.Server/Controllers/ProfilesController.cs
@@ -1,7 +1,19 @@
+using DaNetwork.Server.Services;
+using Microsoft.AspNetCore.Mvc;
+
 namespace DaNetwork.Server.Controllers
 {
-    public class ProfilesController
+    [ApiController]
+    [Route("api/[controller]")]
+
+
+    public class ProfilesController : ControllerBase
     {
+        private readonly AccountService _serviceAcct;
+        private readonly CommentsService _serviceComm;
+        private readonly PostsService _servicePost; 
+        private readonly LikesService _serviceLike;
+        
         
     }
 }
