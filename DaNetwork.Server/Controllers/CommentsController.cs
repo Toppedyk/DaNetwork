94ae581313e490500067c90f3b4d0c6bc6747219
Toppedyk <Tayloroppedyk@yahoo.com>
2021-06-22 14:24:13 -0600

    starting comments

--- DaNetwork.Server/Controllers/CommentsController.cs
+++ DaNetwork.Server/Controllers/CommentsController.cs
@@ -1,7 +1,24 @@
+using DaNetwork.Server.Services;
+using Microsoft.AspNetCore.Mvc;
+
 namespace DaNetwork.Server.Controllers
 {
-    public class CommentsController
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CommentsController: ControllerBase
     {
-        
+        private readonly AccountService _serviceAcct;
+        private readonly CommentsService _serviceComm;
+        private readonly PostsService _servicePost; 
+        private readonly LikesService _serviceLike;
+
+    public CommentsController(AccountService serviceAcct, CommentsService serviceComm, PostsService servicePost, LikesService serviceLike)
+    {
+      _serviceAcct = serviceAcct;
+      _serviceComm = serviceComm;
+      _servicePost = servicePost;
+      _serviceLike = serviceLike;
     }
+  }
 }
