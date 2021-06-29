f4921c616d1ad0e439e3c4b776dc39832cd254e4
Toppedyk <Tayloroppedyk@yahoo.com>
2021-06-29 13:41:38 -0600

    readonly for likes

--- DaNetwork.Server/Controllers/LikesController.cs
+++ DaNetwork.Server/Controllers/LikesController.cs
@@ -1,7 +1,12 @@
+using DaNetwork.Server.Services;
+
 namespace DaNetwork.Server.Controllers
 {
     public class LikesController
     {
-        
+        private readonly AccountService _serviceAcct;
+        private readonly CommentsService _serviceComm;
+        private readonly PostsService _servicePost; 
+        private readonly LikesService _serviceLike;
     }
 }
