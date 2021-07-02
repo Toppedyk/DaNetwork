49eb8ee2103c8b5c6ec36a087a237299a2c8a005
Toppedyk <Tayloroppedyk@yahoo.com>
2021-07-02 12:41:29 -0600

    get likes by profile id

--- DaNetwork.Server/Repositories/LikesRepository.cs
+++ DaNetwork.Server/Repositories/LikesRepository.cs
@@ -1,7 +1,14 @@
+using System;
+using System.Collections.Generic;
+using DaNetwork.Server.Models;
+
 namespace DaNetwork.Server.Repositories
 {
-    public class LikesRepository
+  public class LikesRepository
+  {
+    internal IEnumerable<Like> GetLikesByProfileId(string id)
     {
-        
+      throw new NotImplementedException();
     }
+  }
 }
--- DaNetwork.Server/Services/LikesService.cs
+++ DaNetwork.Server/Services/LikesService.cs
@@ -1,14 +1,22 @@
 using System;
 using System.Collections.Generic;
 using DaNetwork.Server.Models;
+using DaNetwork.Server.Repositories;
 
 namespace DaNetwork.Server.Services
 {
   public class LikesService
   {
+
+    private readonly LikesRepository _repo;
+public LikesService(LikesRepository repo)
+    {
+      _repo = repo;
+    }
+
     internal IEnumerable<Like> GetLikesByProfileId(string id)
     {
-      throw new NotImplementedException();
+      return _repo.GetLikesByProfileId(id);
     }
 
     internal IEnumerable<Like> GetLikesByPostId(int id)
