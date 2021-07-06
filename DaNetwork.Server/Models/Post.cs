0439bb2fa5dbda12077791ce54ee7776fe19403c
Toppedyk <Tayloroppedyk@yahoo.com>
2021-07-06 13:28:16 -0600

    post model defaults

--- DaNetwork.Server/Models/Post.cs
+++ DaNetwork.Server/Models/Post.cs
@@ -8,8 +8,8 @@ namespace DaNetwork.Server.Models
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public string Body { get; set; }
-        public string ImgUrl { get; set; }
-        public int Likes { get; set; }
+        public string ImgUrl { get; set; } = "//placehold.it/200x300";
+        public int Likes { get; set; } = 0;
         public string CreatorId { get; set; }
 
         public Profile Creator { get; set; }
