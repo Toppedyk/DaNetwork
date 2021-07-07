efb991bfdebad262f77670c1efcc6aaf8d4251b8
Toppedyk <Tayloroppedyk@yahoo.com>
2021-07-07 14:08:29 -0600

    get account posts

--- DaNetwork.Client/src/services/PostsService.js
+++ DaNetwork.Client/src/services/PostsService.js
@@ -17,5 +17,11 @@ class PostsService {
     const res = await api.get(`api/profiles/${id}/posts`)
     AppState.accountPosts = res.data.reverse()
   }
+
+  async createPost(post) {
+    await api.post('api/posts', post)
+    await this.getAllPosts()
+    await this.getPostsByAccountId(`api/profiles/${post.creatorId}/posts`)
+  }
 }
 export const postsService = new PostsService()
